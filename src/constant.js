a1f98b9a8db7e84c7ec09e749fb629e880de980b
Himanshu2239 <himanshukumar2239@gmail.com>
2024-12-16 10:44:10 +0530

    fix db-name

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
-export const DB_NAME = "youtubee";
+export const DB_NAME = "youtube";
 export const MAX_LIMIT_OF_DATA = "15000kb";
 export const STORE_STATIC_DATA = "public";
 export const APPLICATION_ROLE = ["admin", "salesperson", "production"];
