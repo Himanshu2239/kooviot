a52331a682c4592d4ac9092c010d0fe96c941216
Himanshu Kumar <96580706+Himanshu2239@users.noreply.github.com>
2025-01-06 10:52:47 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ import cookieParser from "cookie-parser";
 import { MAX_LIMIT_OF_DATA, STORE_STATIC_DATA } from "./constant.js";
 
 const app = express();
-// app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
-app.use(cors())
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+// app.use(cors())
 
 app.use(express.json({ limit: `${MAX_LIMIT_OF_DATA}` })); //accept data : json format
 app.use(express.urlencoded({ extended: true, limit: `${MAX_LIMIT_OF_DATA}` })); //accept data : url
